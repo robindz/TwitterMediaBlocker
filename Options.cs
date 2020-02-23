f5f196405ce3ae25d1c558218e1bc084e03f94a0
Robin De Zwaef <robin.de.zwaef@telenet.be>
2020-02-23 20:46:22 +0100

    Add project

---
+++ Options.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+
+namespace TwitterMediaBlocker
+{
+    public class Options
+    {
+        [Option("accesstoken", Required = true, HelpText = "The AccessToken linked to your Twitter Application.")]
+        public string AccessToken { get; set; }
+
+        [Option("accesstokensecret", Required = true, HelpText = "The AccessTokenSecret linked to your Twitter Application.")]
+        public string AccessTokenSecret { get; set; }
+
+        [Option("consumerkey", Required = true, HelpText = "The ConsumerKey linked to your Twitter Application.")]
+        public string ConsumerKey { get; set; }
+
+        [Option("consumersecret", Required = true, HelpText = "The ConsumerSecret linked to your Twitter Application.")]
+        public string ConsumerSecret { get; set; }
+
+        [Option('p', "photos", Required = false, Default = false, HelpText = "When provided, block users that reply with photos.")]
+        public bool Photos { get; set; }
+
+        [Option('g', "gifs", Required = false, Default = false, HelpText = "When provided, block users that reply with gifs.")]
+        public bool Gifs { get; set; }
+
+        [Option('v', "videos", Required = false, Default = false, HelpText = "When provided, block users that reply with videos.")]
+        public bool Videos { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,148 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Streaming;
+
+namespace TwitterMediaBlocker
+{
+    public class Program
+    {
+        private static string accessToken;
+        private static string accessTokenSecret;
+        private static string consumerKey;
+        private static string consumerSecret;
+        private static bool blockPhotos;
+        private static bool blockGifs;
+        private static bool blockVideos;
+        private static IFilteredStream stream;
+        private static IUser user;
+
+        public static async Task Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (string.IsNullOrEmpty(o.AccessToken)
+                     || string.IsNullOrEmpty(o.AccessTokenSecret)
+                     || string.IsNullOrEmpty(o.ConsumerKey)
+                     || string.IsNullOrEmpty(o.ConsumerSecret))
+                    {
+                        Environment.Exit(0);
+                    }
+
+                    if (!o.Photos && !o.Gifs && !o.Videos)
+                    {
+                        Console.WriteLine("\nYou need to provide at least one type of media to be blocked.");
+                        Console.WriteLine("\t-p | photos");
+                        Console.WriteLine("\t-g | gifs");
+                        Console.WriteLine("\t-v | videos");
+                        Environment.Exit(0);
+                    }
+
+                    accessToken = o.AccessToken;
+                    accessTokenSecret = o.AccessTokenSecret;
+                    consumerKey = o.ConsumerKey;
+                    consumerSecret = o.ConsumerSecret;
+                    blockPhotos = o.Photos;
+                    blockGifs = o.Gifs;
+                    blockVideos = o.Videos;
+                })
+                .WithNotParsed<Options>(error =>
+                {
+                    if (error.Any(x => x.StopsProcessing))
+                        Environment.Exit(0);
+                    if (error.Any(x => x.Tag == ErrorType.MissingRequiredOptionError))
+                        Environment.Exit(0);
+                });
+
+            try
+            {
+                Authenticate();
+                InitializeStream();
+                await stream.StartStreamMatchingAnyConditionAsync();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to authenticate. Are your credentials correct?");
+            }
+        }
+
+        private static void Authenticate()
+        {
+            Auth.SetCredentials(new TwitterCredentials
+            {
+                AccessToken = accessToken,
+                AccessTokenSecret = accessTokenSecret,
+                ConsumerKey = consumerKey,
+                ConsumerSecret = consumerSecret
+            });
+
+            user = User.GetAuthenticatedUser();
+        }
+
+        private static void InitializeStream()
+        {
+            stream = Stream.CreateFilteredStream();
+            stream.AddFollow(user);
+            stream.StreamStopped += StreamStopped;
+            stream.StreamStarted += StreamStarted;
+            stream.MatchingTweetReceived += MatchingTweetReceived;
+        }
+
+        private static void MatchingTweetReceived(object sender, Tweetinvi.Events.MatchedTweetReceivedEventArgs e)
+        {
+            var tweet = e.Tweet;
+            if (tweet.InReplyToUserId == user.Id && tweet.Media.Any())
+            {
+                string mediaType = GetMediaType(tweet);
+                if ((mediaType == "photo" && blockPhotos)
+                 || (mediaType == "video" && blockVideos)
+                 || (mediaType == "gif"   && blockGifs))
+                {
+                    Console.WriteLine($"Blocking @{tweet.CreatedBy.ScreenName} because they replied with a {mediaType} to https://www.twitter.com/i/status/{tweet.InReplyToStatusId}");
+                    User.BlockUser(tweet.CreatedBy);
+                }
+            }
+        }
+
+        private static void RestartStream()
+        {
+            Console.WriteLine("Restarting...");
+            stream.StopStream();
+            InitializeStream();
+            stream.StartStreamMatchingAnyConditionAsync();
+        }
+
+        private static void StreamStarted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Tweet monitoring has been started...");
+        }
+
+        private static void StreamStopped(object sender, Tweetinvi.Events.StreamExceptionEventArgs e)
+        {
+            Console.WriteLine("Tweet monitoring has been stopped...");
+            Console.Write("Error(s): ");
+            if (e != null && e.DisconnectMessage != null)
+                Console.WriteLine(e.DisconnectMessage.Reason);
+            else
+                Console.WriteLine();
+
+            RestartStream();
+        }
+
+        private static string GetMediaType(ITweet tweet)
+        {
+            if (tweet.Media.First().MediaType == "photo")
+                return "photo";
+            if (tweet.Media.First().MediaType == "video")
+                return "video";
+            if (tweet.Media.First().MediaType == "animated_gif")
+                return "gif";
+            return string.Empty;
+        }
+    }
+}
